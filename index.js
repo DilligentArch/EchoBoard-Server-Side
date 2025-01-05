25a70e6971cdff58918d149be66730ef71dac207
unknown <nayebqureshi4@gmail.com>
2025-01-05 20:25:46 +0600

    fixed middlewere bugs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2x9eo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
